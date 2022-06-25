f94e29f6b7ddb5dfdb794464477a57816febdcfa
danhchoigioitre1 <vietanhgalileo@gmail.com>
2022-06-25 13:40:25 +0700

    alert

---
+++ your-folder/lam-bai-tap/src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+function Alert({text,...props}) {
+  return (
+    <div>
+      <h1 {...props}>{text}</h1>
+    </div>
+  )
+
+}
+
+
+function App() {
+  return (
+    <div id='wapper'>
+      <Alert 
+    
+    text="Cảnh báo! Tài nguyên bạn vừa truy cập không tồn tại." 
+    className="alert alert-warning"
+    role="alert"
+    
+    />
+    </div>
+  );
+}
+
+export default App;
